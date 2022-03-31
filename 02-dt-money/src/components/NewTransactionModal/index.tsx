fa5c37f4c5fe5165965727f2fe67f37148ea0015
Felipe Melo <narulee@hotmail.com>
2022-03-30 22:31:51 -0300

    setup miragejs and axios, start modal component

---
+++ 02-dt-money/src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,33 @@
+import Modal from "react-modal";
+import closeImg from '../../assets/images/close.svg'
+import { Container } from "./styles";
+
+Modal.setAppElement("#root")
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button onClick={onRequestClose} className="react-modal-close">
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  )
+}
