22ead56dfc951884c78759f5c293eb1f7e9fc032
Felipe Melo <narulee@hotmail.com>
2022-04-12 23:22:59 -0300

    03-ignews: stripe payment

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStipeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +8,31 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const session = useSession()
+
+  async function handleSubscribe() {
+    if (!session.data) {
+      signIn('github')
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStipeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
