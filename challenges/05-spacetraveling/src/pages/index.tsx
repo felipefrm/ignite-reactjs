4e7ad94b45aac4e94c1b73892f326291a4ac7262
Felipe Melo <narulee@hotmail.com>
2022-04-20 21:13:40 -0300

    05-spacetraveling: home structure and styles

--- challenges/05-spacetraveling/src/pages/index.tsx
+++ challenges/05-spacetraveling/src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 interface Post {
   uid?: string;
@@ -24,9 +27,88 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <strong>Lorem ipsum dolor sit amet.</strong>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            <div className={styles.info}>
+              <span>
+                <FiCalendar />
+                <time>15 Mar 2021</time>
+              </span>
+              <span>
+                <FiUser />
+                <p>Felipe Melo</p>
+              </span>
+            </div>
+          </a>
+        </div>
+
+        <div className={styles.posts}>
+          <a href="#">
+            <strong>Lorem ipsum dolor sit amet.</strong>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            <div className={styles.info}>
+              <span>
+                <FiCalendar />
+                <time>15 Mar 2021</time>
+              </span>
+              <span>
+                <FiUser />
+                <p>Felipe Melo</p>
+              </span>
+            </div>
+          </a>
+        </div>
+
+        <div className={styles.posts}>
+          <a href="#">
+            <strong>Lorem ipsum dolor sit amet.</strong>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            <div className={styles.info}>
+              <span>
+                <FiCalendar />
+                <time>15 Mar 2021</time>
+              </span>
+              <span>
+                <FiUser />
+                <p>Felipe Melo</p>
+              </span>
+            </div>
+          </a>
+        </div>
+
+        <div className={styles.posts}>
+          <a href="#">
+            <strong>Lorem ipsum dolor sit amet.</strong>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            <div className={styles.info}>
+              <span>
+                <FiCalendar />
+                <time>15 Mar 2021</time>
+              </span>
+              <span>
+                <FiUser />
+                <p>Felipe Melo</p>
+              </span>
+            </div>
+          </a>
+        </div>
+
+        <button type='button'>Carregar mais posts</button>
+
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
