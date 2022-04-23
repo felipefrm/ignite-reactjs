8ba267d3a1449cbda81f527c27dfb93892030d14
Felipe Melo <narulee@hotmail.com>
2022-04-22 22:54:12 -0300

    challenge 05: define paths and fallback in getSaticPaths, edit Post type in order to pass the tests, formatting date outside of getSatisProps in order to pass the tests

--- challenges/05-spacetraveling/src/pages/post/[slug].tsx
+++ challenges/05-spacetraveling/src/pages/post/[slug].tsx
@@ -11,12 +11,16 @@ import { formatDate } from '../../utils/formatDate';
 import PostInfo from '../../components/PostInfo';
 import Header from '../../components/Header';
 import { computeReadTime } from '../../utils/computeReadTime';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 
 interface Post {
+  uid: string,
   first_publication_date: string | null;
   data: {
     title: string;
+    subtitle: string;
     banner: {
       url: string;
     };
@@ -35,8 +39,17 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>
+  }
+
   return (
     <>
+      <Head>
+        <title>{post.data.title} | spacetraveling</title>
+      </Head>
       <Header />
       <article>
         <img className={styles.banner} src="https://images.prismic.io/spacetraveling-felipefrm/1b21ee0b-402c-4096-acd7-5d1af22adb76_Banner.png?auto=compress,format" alt="Post Banner" />
@@ -44,7 +57,7 @@ export default function Post({ post }: PostProps) {
           <h1>{post.data.title}</h1>
           <div className={styles.info}>
             <PostInfo
-              date={post.first_publication_date}
+              date={formatDate(post.first_publication_date)}
               author={post.data.author}
               readTime={computeReadTime(post.data.content)}
             />
@@ -53,9 +66,11 @@ export default function Post({ post }: PostProps) {
           {post.data.content.map(content => (
             <section key={content.heading}>
               <h2>{content.heading}</h2>
-              {content.body.map(paragraph => (
-                <p key={Math.random()}>{paragraph.text}</p>
-              ))}
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: RichText.asHtml(content.body)
+                }}
+              />
             </section>
           ))}
 
@@ -69,9 +84,15 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient({});
   const posts = await prismic.getByType('posts');
 
+  const paths = posts.results.map(post => ({
+    params: {
+      slug: post.uid
+    }
+  }))
+
   return {
-    paths: [],
-    fallback: 'blocking'
+    paths: paths,
+    fallback: true
   }
 };
 
@@ -80,27 +101,24 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const { slug } = params
 
-  const response = await prismic.getByUID('posts', String(slug), {})
-
-  const contentArray = response.data.content.map(content => ({
-    heading: content.heading,
-    body: content.body
-  }))
+  const response = await prismic.getByUID('posts', String(slug))
 
   const post: Post = {
-    first_publication_date: formatDate(response.first_publication_date),
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
     data: {
       title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
       banner: {
         url: response.data.banner.url
       },
-      author: response.data.author,
-      content: contentArray
+      content: response.data.content
     }
   }
 
   return {
-    props: {
+    props: { 
       post
     }
   }
