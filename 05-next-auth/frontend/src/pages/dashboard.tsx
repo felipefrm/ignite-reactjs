f3869e94cf15174ca7946a1f7a8997c1abd101d3
Felipe Melo <narulee@hotmail.com>
2022-05-20 23:12:54 -0300

    05-next-auth: permissions and roles control

--- 05-next-auth/frontend/src/pages/dashboard.tsx
+++ 05-next-auth/frontend/src/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 
 import { AuthContext } from "../contexts/AuthContext"
 import { setupAPIClient } from "../services/api"
@@ -6,7 +7,7 @@ import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext)
+  const { user, signOut } = useContext(AuthContext)
 
   useEffect(() => {
     api.get('/me')
@@ -16,8 +17,14 @@ export default function Dashboard() {
 
   return (
     <>
-      <h1>Dashboard</h1>
-      <p>{user?.email}</p>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign Out</button>
+
+      <Can permissions={['metrics.list']} roles={['administrator']}>
+        <div>Métricas</div>
+      </Can>
+
     </>
   )
 }
