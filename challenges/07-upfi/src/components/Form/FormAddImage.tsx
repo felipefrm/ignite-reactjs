a2e88b93b1be4a763b49278d885c36b98ce8764b
Felipe Melo <narulee@hotmail.com>
2022-05-23 22:08:04 -0300

    challenge 07: form input validation

--- challenges/07-upfi/src/components/Form/FormAddImage.tsx
+++ challenges/07-upfi/src/components/Form/FormAddImage.tsx
@@ -18,13 +18,40 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
 
   const formValidations = {
     image: {
-      // TODO REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      required: "Arquivo obrigatório",
+      validate: {
+        lessThan10MB: (image: File) => {
+          return (
+            image[0]?.size < 10000 * 1024 || "O arquivo deve ser menor que 10MB"
+          )
+        }
+        ,
+        acceptedFormats: (image: File) => {
+          return (
+            ['image/jpeg', 'image/png', 'image/gif'].includes(image[0]?.type)
+            ||
+            "Somente são aceitos arquivos PNG, JPEG e GIF"
+          )
+        }
+      }
     },
     title: {
-      // TODO REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      required: "Título obrigatório",
+      minLength: {
+        value: 2,
+        message: "Mínimo de 2 caracteres"
+      },
+      maxLength: {
+        value: 20,
+        message: "Máximo de 20 caracteres"
+      }
     },
     description: {
-      // TODO REQUIRED, MAX LENGTH VALIDATIONS
+      required: "Descrição obrigatória",
+      maxLength: {
+        value: 65,
+        message: "Máximo de 65 caracteres"
+      }
     },
   };
 
@@ -67,20 +94,20 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
           setLocalImageUrl={setLocalImageUrl}
           setError={setError}
           trigger={trigger}
-          // TODO SEND IMAGE ERRORS
-          // TODO REGISTER IMAGE INPUT WITH VALIDATIONS
+          error={errors.image}
+          {...register('image', formValidations.image)}
         />
 
         <TextInput
           placeholder="Título da imagem..."
-          // TODO SEND TITLE ERRORS
-          // TODO REGISTER TITLE INPUT WITH VALIDATIONS
+          error={errors.title}
+          {...register('title', formValidations.title)}
         />
 
         <TextInput
           placeholder="Descrição da imagem..."
-          // TODO SEND DESCRIPTION ERRORS
-          // TODO REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          error={errors.description}
+          {...register('description', formValidations.description)}
         />
       </Stack>
 
